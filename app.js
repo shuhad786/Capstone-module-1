da17efe45d7f4b118900891e936a19ae894c323e
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-15 13:41:11 +0200

    Updated app.js file for linter errors

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-const main = document.querySelector('.main-container');
 const guestSpeaker = document.getElementById('guest-speaker');
 const MguestSpeaker = document.getElementById('Mguest-speaker');
 
-
 const guestsArr = [{
-  name: 'Akira Toriyama',
-  title: 'Creator of Dragon Ball Universe',
-  bio: 'Japanese manga artist and character designer at Funimation studios.',
-  img: './assets/Akira-Toriyama.jpg',
-  },
-  {
-  name: 'Toyotarou',
-  title: 'Lead anime artist of Dragon Ball Universe',
-  bio: 'He has drawn several Dragon Ball-related manga and is best known for illustrating Dragon Ball Super (2015 – present), which is written by series creator Akira Toriyama.',
-  img: './assets/Toyotaro.jpg',
-  },
-  {
-  name: 'Sean Schemmel',
-  title: 'Voice actor of Goku dubbed',
-  bio: 'American voice actor, ADR director and screenwriter',
-  img: './assets/Sean-Schemmel.jpg',
-  },
-  {
-  name: 'Christopher Sabat',
-  title: 'Voice actor of Vegeta, Piccolo, Yamcha and Shenron dubbed',
-  bio: 'Voice actor for English-versions of Japanese anime and video games, including a variety of Dragon Ball characters at Funimation studios',
-  img: './assets/Christopher-Sabat.jpg',
-  },
+name: 'Akira Toriyama',
+title: 'Creator of Dragon Ball Universe',
+bio: 'Japanese manga artist and character designer at Funimation studios.',
+img: './assets/Akira-Toriyama.jpg',
+},
+{
+name: 'Toyotarou',
+title: 'Lead anime artist of Dragon Ball Universe',
+bio: 'He has drawn several Dragon Ball-related manga and is best known for illustrating Dragon Ball Super (2015 – present), which is written by series creator Akira Toriyama.',
+img: './assets/Toyotaro.jpg',
+},
+{
+name: 'Sean Schemmel',
+title: 'Voice actor of Goku dubbed',
+bio: 'American voice actor, ADR director and screenwriter',
+img: './assets/Sean-Schemmel.jpg',
+},
+{
+name: 'Christopher Sabat',
+title: 'Voice actor of Vegeta, Piccolo, Yamcha and Shenron dubbed',
+bio: 'Voice actor for English-versions of Japanese anime and video games, including a variety of Dragon Ball characters at Funimation studios',
+img: './assets/Christopher-Sabat.jpg',
+},
 ];
 
 for (let i = 0; i < guestsArr.length; i += 1) {
@@ -55,36 +53,54 @@ for (let i = 0; i < guestsArr.length; i += 1) {
   bio.innerHTML = guestsArr[i].bio;
   bio.classList.add('bio');
 
-  guestSpeaker.appendChild(container1).appendChild(card1).appendChild(image);
-  guestSpeaker.appendChild(container1).appendChild(card1).appendChild(card2).appendChild(nameElement);
-  guestSpeaker.appendChild(container1).appendChild(card1).appendChild(card2).appendChild(title);
-  guestSpeaker.appendChild(container1).appendChild(card1).appendChild(card2).appendChild(bio);
+  guestSpeaker
+  .appendChild(container1)
+  .appendChild(card1)
+  .appendChild(image);
+  
+  guestSpeaker
+  .appendChild(container1)
+  .appendChild(card1)
+  .appendChild(card2)
+  .appendChild(nameElement);
+
+  guestSpeaker
+  .appendChild(container1)
+  .appendChild(card1)
+  .appendChild(card2)
+  .appendChild(title);
+
+  guestSpeaker
+  .appendChild(container1)
+  .appendChild(card1)
+  .appendChild(card2)
+  .appendChild(bio);
 }
 
 const MguestsArr = [{
-  name: 'Akira Toriyama',
-  title: 'Creator of Dragon Ball Universe',
-  bio: 'Japanese manga artist and character designer at Funimation studios.',
-  img: './assets/Akira-Toriyama.jpg',
-  },
-  {
-  name: 'Toyotarou',
-  title: 'Lead anime artist of Dragon Ball Universe',
-  bio: 'He has drawn several Dragon Ball-related manga and is best known for illustrating Dragon Ball Super (2015 – present), which is written by series creator Akira Toriyama.',
-  img: './assets/Toyotaro.jpg',
-  },
-  {
-  name: 'Sean Schemmel',
-  title: 'Voice actor of Goku dubbed',
-  bio: 'American voice actor, ADR director and screenwriter',
-  img: './assets/Sean-Schemmel.jpg',
-  },
-  {
-  name: 'Christopher Sabat',
-  title: 'Voice actor of Vegeta, Piccolo, Yamcha and Shenron dubbed',
-  bio: 'Voice actor for English-versions of Japanese anime and video games, including a variety of Dragon Ball characters at Funimation studios',
-  img: './assets/Christopher-Sabat.jpg',
-  },
+name: 'Akira Toriyama',
+title: 'Creator of Dragon Ball Universe',
+bio: 'Japanese manga artist and character designer at Funimation studios.',
+img: './assets/Akira-Toriyama.jpg',
+},
+{
+name: 'Toyotarou',
+title: 'Lead anime artist of Dragon Ball Universe',
+bio: 'He has drawn several Dragon Ball-related manga and is best known for illustrating Dragon Ball Super (2015 – present), which is written by series creator Akira Toriyama.',
+img: './assets/Toyotaro.jpg',
+},
+{
+name: 'Sean Schemmel',
+title: 'Voice actor of Goku dubbed',
+bio: 'American voice actor, ADR director and screenwriter',
+img: './assets/Sean-Schemmel.jpg',
+},
+{
+name: 'Christopher Sabat',
+title: 'Voice actor of Vegeta, Piccolo, Yamcha and Shenron dubbed',
+bio: 'Voice actor for English-versions of Japanese anime and video games, including a variety of Dragon Ball characters at Funimation studios',
+img: './assets/Christopher-Sabat.jpg',
+},
 ];
 
 for (let i = 0; i < MguestsArr.length; i += 1) {
@@ -113,8 +129,26 @@ for (let i = 0; i < MguestsArr.length; i += 1) {
   bio.innerHTML = guestsArr[i].bio;
   bio.classList.add('Mbio');
 
-  MguestSpeaker.appendChild(container2).appendChild(card3).appendChild(image);
-  MguestSpeaker.appendChild(container2).appendChild(card3).appendChild(card4).appendChild(nameElement);
-  MguestSpeaker.appendChild(container2).appendChild(card3).appendChild(card4).appendChild(title);
-  MguestSpeaker.appendChild(container2).appendChild(card3).appendChild(card4).appendChild(bio);
+  MguestSpeaker
+  .appendChild(container2)
+  .appendChild(card3)
+  .appendChild(image);
+
+  MguestSpeaker
+  .appendChild(container2)
+  .appendChild(card3)
+  .appendChild(card4)
+  .appendChild(nameElement);
+  
+  MguestSpeaker
+  .appendChild(container2)
+  .appendChild(card3)
+  .appendChild(card4)
+  .appendChild(title);
+  
+  MguestSpeaker
+  .appendChild(container2)
+  .appendChild(card3)
+  .appendChild(card4)
+  .appendChild(bio);
 }
