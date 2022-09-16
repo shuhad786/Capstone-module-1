a1de3393c4557e28c4381e6ce4f56f5fbb881906
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-16 14:41:23 +0200

    Updated layout as per reviewers request

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const guestSpeaker = document.getElementById('guest-speaker');
-const MguestSpeaker = document.getElementById('Mguest-speaker');
 
 const guestsArr = [{
   name: 'Akira Toriyama',
   title: 'Creator of Dragon Ball Universe',
   bio: 'Japanese manga artist and character designer at Funimation studios.',
   img: './assets/Akira-Toriyama.jpg',
+  bg: './assets/chess-board_1.png',
 },
 {
   name: 'Toyotarou',
   title: 'Lead anime artist of Dragon Ball Universe',
-  bio: 'He has drawn several Dragon Ball-related manga and is best known for illustrating Dragon Ball Super (2015 – present), which is written by series creator Akira Toriyama.',
+  bio: 'Is best known for illustrating Dragon Ball Super (2015–present), which is written by series creator Akira Toriyama.',
   img: './assets/Toyotaro.jpg',
+  bg: './assets/chess-board_1.png',
 },
 {
   name: 'Sean Schemmel',
   title: 'Voice actor of Goku dubbed',
   bio: 'American voice actor, ADR director and screenwriter',
   img: './assets/Sean-Schemmel.jpg',
+  bg: './assets/chess-board_1.png',
 },
 {
   name: 'Christopher Sabat',
   title: 'Voice actor of Vegeta, Piccolo, Yamcha and Shenron dubbed',
   bio: 'Voice actor for English-versions of Japanese anime and video games, including a variety of Dragon Ball characters at Funimation studios',
   img: './assets/Christopher-Sabat.jpg',
+  bg: './assets/chess-board_1.png',
+},
+{
+  name: 'Ken Kuraragi',
+  title: 'Father of the Playstation',
+  bio: ' He is known as "The Father of the PlayStation", as he oversaw the development of the original console and its successors and spinoffs',
+  img: './assets/Ken.jpg',
+  bg: './assets/chess-board_1.png',
+},
+{
+  name: 'Lee Byung-chul',
+  title: 'Owner of Samsung',
+  bio: 'He was the founder of the Samsung Group, which is South Koreas largest business group',
+  img: './assets/Lee_Byung-chul.jpg',
+  bg: './assets/chess-board_1.png',
 },
 ];
 
@@ -38,8 +55,12 @@ for (let i = 0; i < guestsArr.length; i += 1) {
   card2.classList.add('col2');
 
   const image = document.createElement('img');
-  image.classList.add('img-fluid', 'guest-image');
+  image.classList.add('guest-image');
   image.src = guestsArr[i].img;
+
+  const bg = document.createElement('img');
+  bg.classList.add('bg-card');
+  bg.src = guestsArr[i].bg;
 
   const nameElement = document.createElement('h4');
   nameElement.innerHTML = guestsArr[i].name;
@@ -54,6 +75,11 @@ for (let i = 0; i < guestsArr.length; i += 1) {
   bio.classList.add('bio');
 
   guestSpeaker
+  .appendChild(container1)
+  .appendChild(card1)
+  .appendChild(bg);
+
+  guestSpeaker
     .appendChild(container1)
     .appendChild(card1)
     .appendChild(image);
@@ -74,81 +100,5 @@ for (let i = 0; i < guestsArr.length; i += 1) {
     .appendChild(container1)
     .appendChild(card1)
     .appendChild(card2)
-    .appendChild(bio);
-}
-
-const MguestsArr = [{
-  name: 'Akira Toriyama',
-  title: 'Creator of Dragon Ball Universe',
-  bio: 'Japanese manga artist and character designer at Funimation studios.',
-  img: './assets/Akira-Toriyama.jpg',
-},
-{
-  name: 'Toyotarou',
-  title: 'Lead anime artist of Dragon Ball Universe',
-  bio: 'He has drawn several Dragon Ball-related manga and is best known for illustrating Dragon Ball Super (2015 – present), which is written by series creator Akira Toriyama.',
-  img: './assets/Toyotaro.jpg',
-},
-{
-  name: 'Sean Schemmel',
-  title: 'Voice actor of Goku dubbed',
-  bio: 'American voice actor, ADR director and screenwriter',
-  img: './assets/Sean-Schemmel.jpg',
-},
-{
-  name: 'Christopher Sabat',
-  title: 'Voice actor of Vegeta, Piccolo, Yamcha and Shenron dubbed',
-  bio: 'Voice actor for English-versions of Japanese anime and video games, including a variety of Dragon Ball characters at Funimation studios',
-  img: './assets/Christopher-Sabat.jpg',
-},
-];
-
-for (let i = 0; i < MguestsArr.length; i += 1) {
-  const container2 = document.createElement('div');
-  container2.classList.add('mobile-grid', 'mt-5');
-
-  const card3 = document.createElement('div');
-  card3.classList.add('row1');
-
-  const card4 = document.createElement('div');
-  card4.classList.add('row2');
-
-  const image = document.createElement('img');
-  image.classList.add('img-fluid', 'guest-image');
-  image.src = guestsArr[i].img;
-
-  const nameElement = document.createElement('h4');
-  nameElement.innerHTML = guestsArr[i].name;
-  nameElement.classList.add('Mname-card');
-
-  const title = document.createElement('h6');
-  title.innerHTML = guestsArr[i].title;
-  title.classList.add('Mtitle-card');
-
-  const bio = document.createElement('p');
-  bio.innerHTML = guestsArr[i].bio;
-  bio.classList.add('Mbio');
-
-  MguestSpeaker
-    .appendChild(container2)
-    .appendChild(card3)
-    .appendChild(image);
-
-  MguestSpeaker
-    .appendChild(container2)
-    .appendChild(card3)
-    .appendChild(card4)
-    .appendChild(nameElement);
-
-  MguestSpeaker
-    .appendChild(container2)
-    .appendChild(card3)
-    .appendChild(card4)
-    .appendChild(title);
-
-  MguestSpeaker
-    .appendChild(container2)
-    .appendChild(card3)
-    .appendChild(card4)
     .appendChild(bio);
 }
